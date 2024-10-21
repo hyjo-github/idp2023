29e28d59696f42f7112ce4cf474985e48196f153
Joni Hyttinen <hyjo+github@proton.me>
2024-10-21 15:00:18 +0300

    Implement signal reader and plot it second by second

--- idp2023_example/signal_analyzer.py
+++ idp2023_example/signal_analyzer.py
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: 2023 Joni Hyttinen <joni.hyttinen@uef.fi>
 #
 # SPDX-License-Identifier: MIT
-
+import csv
 import time
+from collections.abc import Iterator
 from pathlib import Path
 
 import numpy as np
@@ -20,15 +21,44 @@ class SignalAnalyzer:
     def __init__(self):
         self.signal_path = None
         self.running = False
-        self.window_size = 1000
+        self.window_size = 50000
         self.x = 0
         self.x_array = np.zeros((self.window_size,))
-        self.y_array = np.zeros_like(self.x_array)
+        self.y_array = np.zeros_like(self.x_array) * np.nan
+        self.y_max = -np.inf  # Use ridicilous values to force y-scale change
+        self.y_min = np.inf
 
-    def _generate_data_array(self) -> None:
-        self.x_array = np.linspace(self.x, self.x + 100, self.window_size)
-        self.y_array = np.cos(2 * np.pi * 1 / 0.5 * self.x_array)
-        self.x += 0.01
+    def _read_signal(self, chunk_size: int = 1) -> Iterator[np.ndarray]:
+        """
+        Read the signal file in chunks of `chunk_size` rows.
+
+        This method is a generator, and it should be called iteratively, for
+        example in a `while` or a `for` loop. A generator method generates
+        values as they are needed. Consequently, this method does not read
+        the whole file at once, but only `count` rows that then yielded
+        to the outer loop for processing. On the next loop iteration, the
+        execution in this method continues after the yield statement.
+
+        :param chunk_size: the number of rows in a chunk
+        :return: a numpy array of shape (count, 2). The final array may be shorter.
+        """
+        if self.signal_path is None or not self.signal_path.is_file():
+            # This could raise an exception, but then we would have to handle that somewhere.
+            print(f"""signal path "{self.signal_path}" is not a valid file.""")
+            return
+
+        with self.signal_path.open("r") as signal_file:
+            reader = csv.reader(signal_file)
+            # skip column headers ("adc1", "adc2")
+            next(reader)
+            data = np.zeros((chunk_size, 2))
+            i = 0
+            for i, row in enumerate(reader):
+                data[i % chunk_size] = np.array(row, np.uint16)
+                if i > 0 and i % chunk_size == 0:
+                    yield data
+            if i % chunk_size != 0:
+                yield data[: i % chunk_size]
 
     def _start(self, set_axis_y=None):
         """
@@ -36,7 +66,7 @@ class SignalAnalyzer:
         """
         self.running = True
         if set_axis_y:
-            set_axis_y.emit(-1.1, 1.1)
+            set_axis_y.emit(self.y_min, self.y_max)
 
     def stop(self):
         """
@@ -64,16 +94,35 @@ class SignalAnalyzer:
         self._start(set_chart_axis_y)
 
         while self.running:
-            self._generate_data_array()
+            self.x = -self.window_size
 
-            # This signal will now block the thread until chart update is finished
-            if update_chart:
-                update_chart.emit(self.x_array, self.y_array)
+            for chunk in self._read_signal(self.window_size // 10):
+                # Break out from the for-loop if `self.running` has changed
+                if not self.running:
+                    break
 
-            if progress_callback:
-                # This loop has no meaningful progress.
-                # Just report the current left-most x-coordinate.
-                progress_callback.emit(self.x)
+                chunk_size = chunk.shape[0]  # At the end of the file, we don't know how large this is
+                self.x_array = np.linspace(self.x, self.x + self.window_size - 1, self.window_size) / 50000
+                self.y_array = np.roll(self.y_array, -chunk_size)  # roll `self.y_array` back by `chunk_size`
+                self.y_array[-chunk_size:] = chunk[:, 1]  # replace the last `chunk_size` entries with `chunk`
+                self.x += chunk_size
 
-            # Too fast loop will flood the event queue and everything crashes
-            time.sleep(0.01)
+                # Update y-axis min/max if need be:
+                self.y_min = y_min if (y_min := np.nanmin(self.y_array)) < self.y_min else self.y_min
+                self.y_max = y_max if (y_max := np.nanmax(self.y_array)) > self.y_max else self.y_max
+
+                # This signal will now block the thread until chart update is finished
+                if set_chart_axis_y:
+                    set_chart_axis_y.emit(self.y_min, self.y_max)
+
+                # This signal will now block the thread until chart update is finished
+                if update_chart:
+                    update_chart.emit(self.x_array, self.y_array)
+
+                if progress_callback:
+                    # This loop has no meaningful progress.
+                    # Just report the current left-most x-coordinate.
+                    progress_callback.emit(self.x)
+
+            # Stop while-loop when the file ends
+            self.running = False
